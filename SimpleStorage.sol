ba66dcc74d59d3c7da6c7fa277127faf62336411
Yustogaf <140851546+Yustogaf@users.noreply.github.com>
2026-01-12 20:58:14 +0100

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: GPL - 3.0
+pragma solidity >=0.8.0 <0.9.0;
 
+contract SimpleStorage {
+    // State variable to store a positive number
+    uint256 public myNumber;
+
+    // Function to modify the value
+    function setNumber(uint256 _newNumber) public {
+        myNumber = _newNumber;
+    }
+
+    // Function to return the value
+    function getNumber() public view returns (uint256) {
+        return myNumber;
+    }
+}
